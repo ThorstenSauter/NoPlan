5433023f75927c0b218ac924364434fc77da3939
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-01-29 21:13:58 +0100

    Moved the GetToDoRequestValidator to the appropriate request file

--- src/NoPlan.Contracts/Requests/ToDos/V1/GetToDoRequest.cs
+++ src/NoPlan.Contracts/Requests/ToDos/V1/GetToDoRequest.cs
@@ -4,3 +4,10 @@ public record GetToDoRequest
 {
     public Guid Id { get; init; }
 }
+
+public class GetToDoRequestValidator : Validator<DeleteToDoRequest>
+{
+    public GetToDoRequestValidator() =>
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("The ToDo identifier is required");
+}
