a5f38a2e5a85598682ba3b5840727e0b1f464849
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-02-05 00:04:38 +0100

    Added health check support (#11)
    - Added liveness probe at `/health/live`
    - Added readiness probe at `/health/ready`

--- src/NoPlan.Infrastructure/DependencyInjection.cs
+++ src/NoPlan.Infrastructure/DependencyInjection.cs
@@ -5,7 +5,18 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) =>
-        services.AddDbContext<PlannerContext>(options => options.UseCosmos(
-            configuration.GetConnectionString("Default"), configuration.GetValue<string>("DatabaseName")));
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var cosmosConnectionString = configuration.GetConnectionString("Default");
+        var databaseName = configuration.GetValue<string>("DatabaseName");
+        services.AddHealthChecks()
+            .AddCosmosDb(
+                cosmosConnectionString,
+                databaseName,
+                "cosmosdb",
+                timeout: TimeSpan.FromSeconds(15),
+                tags: new[] { "db" });
+
+        return services.AddDbContext<PlannerContext>(options => options.UseCosmos(cosmosConnectionString, databaseName));
+    }
 }
