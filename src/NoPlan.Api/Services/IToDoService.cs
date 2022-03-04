c6963db8a182a2890ca9ab3a4db0c30dfd9fb5ec
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-03-04 18:50:27 +0100

    Adjusted the `ToDoService`
    Added the user identifier as a parameter where necessary to restrict interaction to `ToDo` entities owned by the calling user.

--- src/NoPlan.Api/Services/IToDoService.cs
+++ src/NoPlan.Api/Services/IToDoService.cs
@@ -4,9 +4,9 @@ namespace NoPlan.Api.Services;
 
 public interface IToDoService
 {
-    Task<IEnumerable<ToDo>> GetAllAsync();
-    Task<ToDo?> GetAsync(Guid id);
+    Task<IEnumerable<ToDo>> GetAllAsync(Guid userId);
+    Task<ToDo?> GetAsync(Guid id, Guid userId);
     Task<ToDo> CreateAsync(ToDo newToDo);
     Task<ToDo?> UpdateAsync(ToDo updatedToDo);
-    Task<ToDo?> DeleteAsync(Guid id);
+    Task<ToDo?> DeleteAsync(Guid id, Guid userId);
 }
