e545a98775768b20d51d6578cc77c1c177cc8419
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-03-04 18:50:52 +0100

    Added an extension method for retrieving the user identifier from a `ClaimsPrincipal`

---
+++ src/NoPlan.Api/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using Microsoft.Identity.Web;
+
+namespace NoPlan.Api.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static Guid GetId(this ClaimsPrincipal user) =>
+        Guid.TryParse(user.GetObjectId(), out var guid)
+            ? guid
+            : default;
+}
