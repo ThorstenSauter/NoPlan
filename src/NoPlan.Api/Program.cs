f88ddf6ab5635b681a85824f382fe25f59a39a72
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-03-20 16:17:52 +0100

    Added Serilog as the logging framework (#28)
    - Added `Serilog`bootstrap logger to log startup issues
    - Configured `Serilog` from `IConfiguration`
    - Added `Serilog` request logging middleware
    - Replaced `ApplicationInsights` with its `Serilog` sink

--- src/NoPlan.Api/Program.cs
+++ src/NoPlan.Api/Program.cs
@@ -6,75 +6,98 @@ using Microsoft.Identity.Web;
 using NoPlan.Api.Options;
 using NoPlan.Api.Services;
 using NoPlan.Infrastructure.Data;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder();
-var configuration = builder.Configuration;
-builder.Host.ConfigureAppConfiguration(config =>
-    config.AddAzureAppConfiguration(options =>
-    {
-        var appConfigurationOptions = configuration.GetSection(AppConfigurationOptions.SectionName).Get<AppConfigurationOptions>();
-        var isDevelopment = builder.Environment.IsDevelopment();
-        var credential = isDevelopment
-            ? new DefaultAzureCredential()
-            : new(new DefaultAzureCredentialOptions { ManagedIdentityClientId = configuration.GetValue<string>("ManagedIdentityClientId") });
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-        options.Connect(appConfigurationOptions.EndPoint, credential);
-        options.ConfigureKeyVault(c => c.SetCredential(credential));
-        var label = isDevelopment ? "dev" : "prod";
-        options.Select(KeyFilter.Any, label);
-        options.ConfigureRefresh(refreshOptions =>
+try
+{
+    var builder = WebApplication.CreateBuilder();
+    var configuration = builder.Configuration;
+    builder.Host.ConfigureAppConfiguration(config =>
+        config.AddAzureAppConfiguration(options =>
         {
-            refreshOptions.SetCacheExpiration(TimeSpan.FromSeconds(appConfigurationOptions.RefreshInterval));
-            refreshOptions.Register("Sentinel", label, true);
-        });
-    }));
+            var appConfigurationOptions = configuration.GetSection(AppConfigurationOptions.SectionName).Get<AppConfigurationOptions>();
+            var isDevelopment = builder.Environment.IsDevelopment();
+            var credential = isDevelopment
+                ? new DefaultAzureCredential()
+                : new(new DefaultAzureCredentialOptions { ManagedIdentityClientId = configuration.GetValue<string>("ManagedIdentityClientId") });
 
-builder.Services
-    .AddFastEndpoints()
-    .AddSwaggerDoc(maxEndpointVersion: 1, settings: s =>
+            options.Connect(appConfigurationOptions.EndPoint, credential);
+            options.ConfigureKeyVault(c => c.SetCredential(credential));
+            var label = isDevelopment ? "dev" : "prod";
+            options.Select(KeyFilter.Any, label);
+            options.ConfigureRefresh(refreshOptions =>
+            {
+                refreshOptions.SetCacheExpiration(TimeSpan.FromSeconds(appConfigurationOptions.RefreshInterval));
+                refreshOptions.Register("Sentinel", label, true);
+            });
+        }));
+
+    builder.Services
+        .AddFastEndpoints()
+        .AddSwaggerDoc(maxEndpointVersion: 1, settings: s =>
+        {
+            s.DocumentName = "Release v1";
+            s.Title = "NoPlan API";
+            s.Version = "v1";
+        })
+        .AddInfrastructure(configuration)
+        .AddAzureAppConfiguration()
+        .AddScoped<IToDoService, ToDoService>()
+        .AddSingleton<IDateTimeProvider, DateTimeProvider>()
+        .AddAuthorization(options => options.AddPolicy("User", b => b.RequireScope("User")))
+        .AddMicrosoftIdentityWebApiAuthentication(configuration);
+
+    builder.Logging.ClearProviders();
+    builder.Host.UseSerilog((ctx, cfg) =>
+        cfg.ReadFrom.Configuration(ctx.Configuration)
+            .Enrich.WithProperty("ApplicationName", "NoPlan.Api")
+            .Enrich.WithProperty("Version", typeof(Program).Assembly.GetName().Version));
+
+    var app = builder.Build();
+    EnsureDatabaseCreated(app);
+
+    app.UseAzureAppConfiguration();
+    app.UseSerilogRequestLogging();
+    app.UseRouting();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.UseFastEndpoints(c =>
     {
-        s.DocumentName = "Release v1";
-        s.Title = "NoPlan API";
-        s.Version = "v1";
-    })
-    .AddInfrastructure(configuration)
-    .AddAzureAppConfiguration()
-    .AddScoped<IToDoService, ToDoService>()
-    .AddSingleton<IDateTimeProvider, DateTimeProvider>()
-    .AddAuthorization(options => options.AddPolicy("User", b => b.RequireScope("User")))
-    .AddMicrosoftIdentityWebApiAuthentication(configuration);
+        c.RoutingOptions = o => o.Prefix = "api";
+        c.VersioningOptions = o =>
+        {
+            o.Prefix = "v";
+            o.DefaultVersion = 1;
+            o.SuffixedVersion = false;
+        };
+    });
 
-var app = builder.Build();
-EnsureDatabaseCreated(app);
-
-app.UseAzureAppConfiguration();
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseFastEndpoints(c =>
-{
-    c.RoutingOptions = o => o.Prefix = "api";
-    c.VersioningOptions = o =>
+    if (app.Environment.IsDevelopment())
     {
-        o.Prefix = "v";
-        o.DefaultVersion = 1;
-        o.SuffixedVersion = false;
-    };
-});
+        app.UseOpenApi();
+        app.UseSwaggerUi3(s => s.ConfigureDefaults());
+    }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseOpenApi();
-    app.UseSwaggerUi3(s => s.ConfigureDefaults());
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapHealthChecks("/health/ready", new() { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse });
+        endpoints.MapHealthChecks("/health/live", new() { Predicate = _ => false, ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse });
+    });
+
+    app.Run();
 }
-
-app.UseEndpoints(endpoints =>
+catch (Exception ex)
 {
-    endpoints.MapHealthChecks("/health/ready", new() { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse });
-    endpoints.MapHealthChecks("/health/live", new() { Predicate = _ => false, ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse });
-});
-
-app.Run();
+    Log.Fatal(ex, "Encountered a fatal error, shutting down");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
 void EnsureDatabaseCreated(IHost webApplication)
 {
