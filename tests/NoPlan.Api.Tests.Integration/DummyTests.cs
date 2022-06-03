2ecee4e8a7b43813f07e10453e2f7bfffecb3adc
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-06-03 02:19:16 +0200

    Scaffolded unit and integration test projects

---
+++ tests/NoPlan.Api.Tests.Integration/DummyTests.cs
@@ -0,0 +1,11 @@
+﻿namespace NoPlan.Api.Tests.Integration;
+
+public sealed class DummyTests
+{
+    [Fact]
+    public void Dummy_ShouldPass() =>
+        // Arrange
+        // Act
+        // Assert
+        true.Should().BeTrue();
+}
---
+++ tests/NoPlan.Api.Tests.Unit/DummyTests.cs
@@ -0,0 +1,11 @@
+﻿namespace NoPlan.Api.Tests.Unit;
+
+public sealed class DummyTests
+{
+    [Fact]
+    public void Dummy_ShouldPass() =>
+        // Arrange
+        // Act
+        // Assert
+        true.Should().BeTrue();
+}
