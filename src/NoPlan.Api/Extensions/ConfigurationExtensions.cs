b647fb60c611a4eece7817678d5eeaa6966f3967
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-06-15 00:03:56 +0200

    Upgraded packages for .NET 7 Preview 5 (#46)
    * Upgraded packages for .NET 7 Preview 5
    
    ## Changes
    - Moved to `LangVersion` `preview` for all projects
    
    ## Dependencies:
    - Upgraded `Microsoft.EntityFrameworkCore` to `v7.0.0-preview.5.22302.2`
    - Upgraded `Microsoft.EntityFrameworkCore.Cosmos` to `v7.0.0-preview.5.22302.2`
    - Upgraded `Microsoft.EntityFrameworkCore.Design` to `v7.0.0-preview.5.22302.2`
    - Upgraded `Microsoft.EntityFrameworkCore.Design` to `v7.0.0-preview.5.22302.2`
    - Upgraded `Microsoft.Extensions.Configuration.Binder` to `v7.0.0-preview.5.22301.12`
    
    * Added generic way of adding `IOptions` from a config section
    
    - Removed closure in `AddAzureAppConfiguration` call
    - Added interface for strongly typed `IOptions` with a section name
    - Added a generic way of registering strongly typed `IOptions` from a section of `IConfiguration` using static abstract interface members for the section name
    - Registered `AppConfigurationOptions` with dependency injection
    - Added const value for default refresh interval in `AppConfigurationOptions`

---
+++ src/NoPlan.Api/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Options;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigurationExtensions
+{
+    public static IServiceCollection AddSectionedOptions<TOptions>(this IServiceCollection services, IConfiguration configuration)
+        where TOptions : class, IOptionsSectionDefinition
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        return services.Configure<TOptions>(configuration.GetSection(TOptions.SectionName));
+    }
+}
