1c030a34d46f889f1d598305e6c072038f32ded6
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-08-11 23:23:21 +0200

    Added `WebApplicationFactory` including authentication using the ROPC flow

---
+++ tests/NoPlan.Api.Tests.Integration/Authentication/TokenResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NoPlan.Api.Tests.Integration.Authentication;
+
+public sealed class TokenResponse
+{
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; } = null!;
+}
---
+++ tests/NoPlan.Api.Tests.Integration/Authentication/UserAuthenticationSettings.cs
@@ -0,0 +1,40 @@
+﻿namespace NoPlan.Api.Tests.Integration.Authentication;
+
+public sealed class UserAuthenticationSettings
+{
+    /// <summary>
+    ///     The URL to acquire the access token from, e.g.
+    ///     https://login.microsoftonline.com/your-aad-tenant-id/oauth2/v2.0/token
+    /// </summary>
+    public string TokenUrl => $"https://login.microsoftonline.com/{TenantId}/oauth2/v2.0/token";
+
+    /// <summary>
+    ///     The tenant id.
+    /// </summary>
+    public string TenantId { get; set; } = null!;
+
+    /// <summary>
+    ///     Client id / application id for the registered test app.
+    /// </summary>
+    public string ClientId { get; set; } = null!;
+
+    /// <summary>
+    ///     Secret for the registered test app.
+    /// </summary>
+    public string ClientSecret { get; set; } = null!;
+
+    /// <summary>
+    ///     Username of a user registered in AAD. Should be from a test tenant, not a production one. Cannot have MFA enabled.
+    /// </summary>
+    public string Username { get; set; } = null!;
+
+    /// <summary>
+    ///     Password of the user.
+    /// </summary>
+    public string Password { get; set; } = null!;
+
+    /// <summary>
+    ///     The application audience.
+    /// </summary>
+    public string Audience { get; set; } = null!;
+}
