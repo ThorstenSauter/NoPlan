2843275a9711b80c9a9f1e5991e95aafd811b357
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-08-14 00:23:27 +0200

    Added unit tests for `FluentValidation` validators
    Added a dependency on `Bogus` `v34.0.2`

---
+++ tests/NoPlan.Api.Tests.Unit/Validators/V1/ToDos/CreateToDoRequestValidatorTest.cs
@@ -0,0 +1,90 @@
+﻿using FluentValidation.TestHelper;
+using NoPlan.Api.Tests.Unit.Fakers;
+using NoPlan.Contracts.Requests.V1.ToDos;
+
+namespace NoPlan.Api.Tests.Unit.Validators.V1.ToDos;
+
+public sealed class CreateToDoRequestValidatorTest : TestWithFakes
+{
+    private readonly CreateToDoRequestValidator _sut;
+
+    public CreateToDoRequestValidatorTest() =>
+        _sut = new();
+
+    [Fact]
+    public void Validate_ShouldFail_WhenTitleIsEmpty()
+    {
+        // Arrange
+        var request = CreateRequestFaker.Clone()
+            .RuleFor(x => x.Title, _ => "")
+            .Generate();
+
+        // Act
+        var result = _sut.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Title)
+            .Only();
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenTitleIsTooShort()
+    {
+        // Arrange
+        var request = CreateRequestFaker.Clone()
+            .RuleFor(x => x.Title, _ => "ab")
+            .Generate();
+
+        // Act
+        var result = _sut.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Title)
+            .Only();
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenDescriptionIsEmpty()
+    {
+        // Arrange
+        var request = CreateRequestFaker.Clone()
+            .RuleFor(x => x.Description, _ => "")
+            .Generate();
+
+        // Act
+        var result = _sut.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Description)
+            .Only();
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenTagsAreNull()
+    {
+        // Arrange
+        var request = CreateRequestFaker.Clone()
+            .RuleFor(x => x.Tags, _ => null!)
+            .Generate();
+
+        // Act
+        var result = _sut.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Tags)
+            .Only();
+    }
+
+    [Fact]
+    public void Validate_ShouldPass_WhenRequestIsCompletelyValid()
+    {
+        // Arrange
+        var request = CreateRequestFaker.Generate();
+
+        // Act
+        var result = _sut.TestValidate(request);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
---
+++ tests/NoPlan.Api.Tests.Unit/Validators/V1/ToDos/UpdateToDoRequestValidatorTest.cs
@@ -0,0 +1,106 @@
+﻿using FluentValidation.TestHelper;
+using NoPlan.Api.Tests.Unit.Fakers;
+using NoPlan.Contracts.Requests.V1.ToDos;
+
+namespace NoPlan.Api.Tests.Unit.Validators.V1.ToDos;
+
+public sealed class UpdateToDoRequestValidatorTest : TestWithFakes
+{
+    private readonly UpdateToDoRequestValidator _sut;
+
+    public UpdateToDoRequestValidatorTest() =>
+        _sut = new();
+
+    [Fact]
+    public void Validate_ShouldFail_WhenIdIsEmpty()
+    {
+        // Arrange
+        var request = UpdateRequestFaker.Clone()
+            .RuleFor(x => x.Id, _ => Guid.Empty)
+            .Generate();
+
+        // Act
+        var result = _sut.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Id)
+            .Only();
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenTitleIsEmpty()
+    {
+        // Arrange
+        var request = UpdateRequestFaker.Clone()
+            .RuleFor(x => x.Title, _ => "")
+            .Generate();
+
+        // Act
+        var result = _sut.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Title)
+            .Only();
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenTitleIsTooShort()
+    {
+        // Arrange
+        var request = UpdateRequestFaker.Clone()
+            .RuleFor(x => x.Title, _ => "ab")
+            .Generate();
+
+        // Act
+        var result = _sut.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Title)
+            .Only();
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenDescriptionIsEmpty()
+    {
+        // Arrange
+        var request = UpdateRequestFaker.Clone()
+            .RuleFor(x => x.Description, _ => "")
+            .Generate();
+
+        // Act
+        var result = _sut.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Description)
+            .Only();
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenTagsAreNull()
+    {
+        // Arrange
+        var request = UpdateRequestFaker.Clone()
+            .RuleFor(x => x.Tags, _ => null!)
+            .Generate();
+
+        // Act
+        var result = _sut.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Tags)
+            .Only();
+    }
+
+    [Fact]
+    public void Validate_ShouldPass_WhenRequestIsCompletelyValid()
+    {
+        // Arrange
+        var request = UpdateRequestFaker.Generate();
+
+        // Act
+        var result = _sut.TestValidate(request);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
