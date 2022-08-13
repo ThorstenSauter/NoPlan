b603d38362fcaedbdff1bcc87a5ac165e79dcccd
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-08-13 15:20:15 +0200

    Switched link to permalink

--- src/NoPlan.Api/HeathChecks/JsonHealthCheckResponseWriter.cs
+++ src/NoPlan.Api/HeathChecks/JsonHealthCheckResponseWriter.cs
@@ -7,7 +7,7 @@ namespace NoPlan.Api.HeathChecks;
 
 /// <summary>
 ///     This writer has been adapted from the existing implementation at
-///     https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks/blob/master/src/HealthChecks.UI.Client/UIResponseWriter.cs
+///     https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks/blob/4f29f80c09e27740e242521a5e07d0b427206f43/src/HealthChecks.UI.Client/UIResponseWriter.cs
 ///     in order to drop the otherwise unnecessary dependency on the <c>AspNetCore.HealthChecks.UI.Client</c> package.
 /// </summary>
 internal static class JsonHealthCheckResponseWriter
