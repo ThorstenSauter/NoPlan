4057de6e74a54f7a325ae5bc6c118e2c1ce1a5c6
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-08-13 15:35:55 +0200

    Added own health check response writer (#106)
    * Added own implementation of a health check response writer
    
    The implementation is copied and adapted from the [AspNetCore.HealthChecks.UI.Clien](https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks/blob/4f29f80c09e27740e242521a5e07d0b427206f43/src/HealthChecks.UI.Client/UIResponseWriter.cs) implementation.
    
    * Removed dependency and switched to new writer
    
    * Switched link to permalink

---
+++ src/NoPlan.Api/HeathChecks/JsonHealthCheckResponseWriter.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace NoPlan.Api.HeathChecks;
+
+/// <summary>
+///     This writer has been adapted from the existing implementation at
+///     https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks/blob/4f29f80c09e27740e242521a5e07d0b427206f43/src/HealthChecks.UI.Client/UIResponseWriter.cs
+///     in order to drop the otherwise unnecessary dependency on the <c>AspNetCore.HealthChecks.UI.Client</c> package.
+/// </summary>
+internal static class JsonHealthCheckResponseWriter
+{
+    private const string DefaultResponseContentType = "application/json";
+    private static readonly byte[] EmptyResponse = "{}"u8.ToArray();
+    private static readonly Lazy<JsonSerializerOptions> Options = new(CreateJsonOptions);
+
+    public static async Task WriteResponse(HttpContext httpContext, HealthReport? report)
+    {
+        if (report is null)
+        {
+            await httpContext.Response.BodyWriter.WriteAsync(EmptyResponse);
+            return;
+        }
+
+        httpContext.Response.ContentType = DefaultResponseContentType;
+        await JsonSerializer.SerializeAsync(httpContext.Response.Body, new MappedHealthReport(report), Options.Value);
+    }
+
+    private static JsonSerializerOptions CreateJsonOptions()
+    {
+        var options = new JsonSerializerOptions
+        {
+            AllowTrailingCommas = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        };
+
+        options.Converters.Add(new JsonStringEnumConverter());
+        return options;
+    }
+}
+
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+internal sealed class MappedHealthReport
+{
+    public MappedHealthReport(HealthReport healthReport)
+    {
+        ArgumentNullException.ThrowIfNull(healthReport);
+
+        Entries = new();
+        TotalDuration = healthReport.TotalDuration;
+        Status = healthReport.Status;
+
+        foreach (var (name, reportEntry) in healthReport.Entries)
+        {
+            var entry = new MappedHealthReportEntry
+            {
+                Data = reportEntry.Data,
+                Description = reportEntry.Description,
+                Duration = reportEntry.Duration,
+                Status = reportEntry.Status,
+                Tags = reportEntry.Tags
+            };
+
+            if (reportEntry.Exception is not null)
+            {
+                var message = reportEntry.Exception?.Message;
+                entry.Exception = message;
+                entry.Description = reportEntry.Description ?? message;
+            }
+
+            Entries.Add(name, entry);
+        }
+    }
+
+    public HealthStatus Status { get; }
+    public TimeSpan TotalDuration { get; }
+
+    // ReSharper disable once CollectionNeverQueried.Global
+    public Dictionary<string, MappedHealthReportEntry> Entries { get; }
+}
+
+internal sealed class MappedHealthReportEntry
+{
+    public required IReadOnlyDictionary<string, object>? Data { get; init; }
+    public required string? Description { get; set; }
+    public required TimeSpan Duration { get; init; }
+    public string? Exception { get; set; }
+    public required HealthStatus Status { get; init; }
+    public required IEnumerable<string>? Tags { get; init; }
+}
