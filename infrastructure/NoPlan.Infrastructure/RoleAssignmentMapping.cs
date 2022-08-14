f0f712c5b7d48d73e3e6b01274866ecfee29c429
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-08-14 16:37:13 +0200

    Added role assignments for RBAC (#113)
    ## Changes
    - Removed protection from all resources
    - Added abstractions to make role assignments less verbose (e.g. role definitions identifiers)

---
+++ infrastructure/NoPlan.Infrastructure/RoleAssignmentMapping.cs
@@ -0,0 +1,9 @@
+﻿using Pulumi;
+
+namespace NoPlan.Infrastructure;
+
+public sealed record RoleAssignmentMapping(string Name, Principal Principal, string RoleName, CustomResource Resource)
+{
+    public string RoleDefinitionId(RoleDefinitions definitions) =>
+        definitions[RoleName];
+}
---
+++ infrastructure/NoPlan.Infrastructure/RoleAssignments.cs
@@ -0,0 +1,20 @@
+﻿using Pulumi.AzureNative.Authorization;
+
+namespace NoPlan.Infrastructure;
+
+public sealed class RoleAssignments
+{
+    public List<RoleAssignment> Assignments { get; }
+
+    public RoleAssignments(IEnumerable<RoleAssignmentMapping> roleAssignments, RoleDefinitions roleMappings) =>
+        Assignments = roleAssignments.Select(r =>
+            new RoleAssignment($"{r.Principal.Name}-{r.Resource.GetResourceName()}-{r.RoleName}",
+                new()
+                {
+                    PrincipalId = r.Principal.Id,
+                    PrincipalType = r.Principal.Type,
+                    RoleAssignmentName = r.Name,
+                    RoleDefinitionId = r.RoleDefinitionId(roleMappings),
+                    Scope = r.Resource.Id
+                })).ToList();
+}
