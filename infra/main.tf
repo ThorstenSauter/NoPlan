83f21733b2c01ed5fb7ecf120c6bb676fa7eb716
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-10-13 16:17:59 +0200

    Updated dependencies
    # CI/CD changes
    
    - Updated `azurerm` Terraform provider to `v3.26.0`
    - Updated `setup-dotnet` GitHub Action to `v3`
    
    # Dependencies
    
    - Updated `Azure.Messaging.ServiceBus` to `v7.11.0`
    - Updated `FastEndpoints` to `v5.2.0-beta28`
    - Updated `FastEndpoints.Generator` to `v5.2.0-beta18`
    - Updated `FastEndpoints.Swagger` to `v5.2.0-beta28`
    - Updated `Microsoft.AspNetCore.Mvc.Testing` to `v7.0.0-rc.2.22476.2`
    - Updated `Microsoft.EntityFrameworkCore` to `v7.0.0-rc.2.22472.11`
    - Updated `Microsoft.EntityFrameworkCore.Design` to `v7.0.0-rc.2.22472.11`
    - Updated `Microsoft.EntityFrameworkCore.SqlServer` to `v7.0.0-rc.2.22472.11`
    - Updated `Microsoft.Extensions.Configuration.Binder` to `v7.0.0-rc.2.22472.3`
    - Updated `Microsoft.NET.Test.Sdk` to `v17.3.2`

--- infra/main.tf
+++ infra/main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.22.0"
+      version = "3.26.0"
     }
   }
 
   backend "azurerm" {
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.3.0"
 }
 
 data "azurerm_client_config" "current" {}
