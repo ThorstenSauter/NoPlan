e3abe36c9707ab032ee51e98f14f470ee11cd4d0
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-11-19 01:04:15 +0100

    Updated dependencies
    # Changes
    
    - Enabled previously disabled Swagger integration
    
    # Terraform
    
    - Updated `azurerm` provider to `v3.32.0`
    
    # NuGet packages
    
    - Updated `FastEndpoints` to `v5.4.1`
    - Updated `FastEndpoints.Generator` to `v5.4.1`
    - Updated `FastEndpoints.Swagger` to `v5.4.1`
    - Updated `Microsoft.Identity.Web` to `v1.25.7`

--- src/NoPlan.Api/Program.cs
+++ src/NoPlan.Api/Program.cs
@@ -1,3 +1,4 @@
+using FastEndpoints.Swagger;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
@@ -22,12 +23,12 @@ try
 
     builder.Services
         .AddFastEndpoints(options => options.SourceGeneratorDiscoveredTypes = DiscoveredTypes.All)
-        // .AddSwaggerDoc(maxEndpointVersion: 1, shortSchemaNames: true, settings: s =>
-        // {
-        //     s.DocumentName = "Release v1";
-        //     s.Title = "NoPlan API";
-        //     s.Version = "v1";
-        // })
+        .AddSwaggerDoc(maxEndpointVersion: 1, shortSchemaNames: true, settings: s =>
+        {
+            s.DocumentName = "Release v1";
+            s.Title = "NoPlan API";
+            s.Version = "v1";
+        })
         .AddInfrastructure(configuration)
         .AddScoped<IToDoService, ToDoService>()
         .AddSingleton<IDateTimeProvider, DateTimeProvider>()
@@ -83,11 +84,11 @@ try
         c.Versioning.PrependToRoute = true;
     });
 
-    // if (app.Environment.IsDevelopment())
-    // {
-    //     app.UseOpenApi();
-    //     app.UseSwaggerUi3(s => s.ConfigureDefaults());
-    // }
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseOpenApi();
+        app.UseSwaggerUi3(s => s.ConfigureDefaults());
+    }
 
     app.MapHealthChecks("/health/ready", new() { ResponseWriter = JsonHealthCheckResponseWriter.WriteResponse });
     app.MapHealthChecks("/health/live", new() { Predicate = _ => false, ResponseWriter = JsonHealthCheckResponseWriter.WriteResponse });
