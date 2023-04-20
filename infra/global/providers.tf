f1ea7cc5001cbf8dbaa67c9269bf720d5794f1a3
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2023-04-20 19:08:36 +0200

    Switched to pessimistic versioning for all providers

--- infra/global/providers.tf
+++ infra/global/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.37.2"
+      version = "~> 2.37.2"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.52.0"
+      version = "~> 3.52.0"
     }
     tfe = {
       source  = "hashicorp/tfe"
