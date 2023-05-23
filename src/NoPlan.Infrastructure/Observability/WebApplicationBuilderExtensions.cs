4fa5fcc448f87dfe423589102af22c2890e734dd
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2023-05-23 14:39:23 +0200

    Enrich tracing with IP and User ID (#441)
    In order to analyze traces, additional data is being recorded.

--- src/NoPlan.Infrastructure/Observability/WebApplicationBuilderExtensions.cs
+++ src/NoPlan.Infrastructure/Observability/WebApplicationBuilderExtensions.cs
@@ -1,13 +1,16 @@
-﻿using Azure.Identity;
+﻿using System.Reflection;
+using Azure.Identity;
 using Azure.Monitor.OpenTelemetry.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web;
 using OpenTelemetry.Instrumentation.AspNetCore;
 using OpenTelemetry.Instrumentation.Http;
 using OpenTelemetry.Instrumentation.SqlClient;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
 namespace NoPlan.Infrastructure.Observability;
@@ -28,7 +31,15 @@ public static class WebApplicationBuilderExtensions
 
     private static IServiceCollection ConfigureInstrumentation(this IServiceCollection services) =>
         services
-            .Configure<AspNetCoreInstrumentationOptions>(options => options.RecordException = true)
+            .Configure<AspNetCoreInstrumentationOptions>(options =>
+            {
+                options.RecordException = true;
+                options.EnrichWithHttpRequest = (activity, request) =>
+                {
+                    activity.SetTag("http.client_ip", request.HttpContext.Connection.RemoteIpAddress);
+                    activity.SetTag("enduser.id", request.HttpContext.User.GetObjectId());
+                };
+            })
             .Configure<HttpClientInstrumentationOptions>(options => options.RecordException = true)
             .Configure<SqlClientInstrumentationOptions>(options =>
             {
@@ -38,6 +49,16 @@ public static class WebApplicationBuilderExtensions
 
     private static IServiceCollection ConfigureTracing(this IServiceCollection services)
     {
+        var resourceAttributes = new Dictionary<string, object>
+        {
+            { "service.name", "NoPlan API" },
+            { "service.namespace", "NoPlan" },
+            { "service.version", Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "unknown" }
+        };
+
+        services.ConfigureOpenTelemetryTracerProvider((sp, builder) =>
+            builder.ConfigureResource(resourceBuilder => resourceBuilder.AddAttributes(resourceAttributes)));
+
         // Workaround due to an issue with the OTLP exporter registering services after the service provider has been built:
         // https://github.com/Azure/azure-sdk-for-net/issues/36339#issuecomment-1552242653
         services.AddOpenTelemetry().WithTracing(builder =>
